fe3d7d964add589151911bd43ce032755926678a
Suvo Ghosh <suvosh663@gmail.com>
2025-05-26 21:02:45 +0530

    Refactor server setup and add HTTP server implementation

--- worker-server/app.js
+++ worker-server/app.js
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
 import { publishUpdate } from './nats/publisher.js';
 import './scheduler/cronJob.js';
+import http from 'http';
 
 dotenv.config();
-console.log('🚀 Worker server running...');
+
+// Dummy server just to keep Render happy
+const PORT = process.env.PORT || 3001;
+http.createServer((req, res) => {
+    res.end('Worker server is running...');
+}).listen(PORT, () => {
+    console.log(`🚀 Worker server running on port ${PORT}`);
+});
